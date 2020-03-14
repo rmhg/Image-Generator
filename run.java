e8988d2dd18411eeb4dc128642753fa2a2cdb576
rmhg <39510036+rmhg@users.noreply.github.com>
2020-03-14 18:13:32 +0530

    Add files via upload

---
+++ run.java
@@ -0,0 +1,10 @@
+import backend.Conn;
+import backend.GetImg;
+import frontend.search;
+
+public class run {
+   public static void main(String[] args)
+   {
+	   new search(args[0],args[1]);
+   }
+}
